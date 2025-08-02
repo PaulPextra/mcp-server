452bea0c13c3daeda0ffa06be2eccb26aa48d734
Paul Okoli <paulokoli7@gmail.com>
2025-08-02 17:40:18 -0500

    fix: mcp-server

--- server-mcp.js
+++ server-mcp.js
@@ -9,7 +9,7 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 8000;
-const SERVER_BASE_URL = process.env.SERVER_BASE_URL || 'https://10.0.1.35:3000';
+const SERVER_BASE_URL = process.env.SERVER_BASE_URL || 'http://10.0.1.35:3000';
 
 if (!SERVER_BASE_URL) {
   console.error('BASE_URL is not set in environment variables.');
